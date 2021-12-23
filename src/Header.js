1b69194b1b95f36d7766b8d2fb69c2aa510a40ce
Patrick Yambrick <patrickyambrick@gmail.com>
2021-12-23 17:34:43 -0500

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+  return(
+    <div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Contact Us</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,7 @@
+function Main() {
+  return(
+    <div>Lorem ipsum</div>
+  );
+}
+
+export default Main;
